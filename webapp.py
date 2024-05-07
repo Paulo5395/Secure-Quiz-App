02598cb02df245d4641f81b537062ba9a2959230
Paulo5395 <paulolange805@gmail.com>
2024-05-06 20:18:51 -0700

    Created new pages, removed nav bar, redirects to home on back button

--- webapp.py
+++ webapp.py
@@ -18,25 +18,93 @@ def startOver():
 
 @app.route("/p1")
 def render_page1():
-    page_number = 1
-
-    return render_template("page1.html", page_number = page_number)
+    if session.get("answer1") is not None: #check if there is already a value for "answer1"
+            session.clear()
+            return redirect("/")
+    else:
+        return render_template("page1.html")
 
 @app.route("/p2", methods=['GET','POST'])
 def render_page2():
-    session["answer1"]=request.form['optradio']
 
-    page_number = 2
-
-    return render_template("page2.html")
-
+    if request.method == "POST":
+        if session.get("answer1") is not None: #check if there is already a value for "answer1"
+            session.clear()
+            return redirect("/")
+        else:
+            if 'optradio' in request.form:
+                session["answer1"]=request.form['optradio']
+            else:
+                session["answer1"] = "None"
+        
+        return render_template("page2.html")
+    else:
+        session.clear()
+        return redirect("/")
+    
 @app.route("/p3", methods=['GET','POST'])
 def render_page3():
-    page_number = 3
 
-    session["answer2"]=request.form['optradio']
-
-    return render_template("page3.html", page_number = page_number)
+    if request.method == "POST":
+        if session.get("answer2") is not None: #check if there is already a value for "answer1"
+            session.clear()
+            return redirect("/")
+        else:
+            if 'optradio' in request.form:
+                session["answer2"]=request.form['optradio']
+            else:
+                session["answer2"] = "None"
+        
+        return render_template("page3.html")
+    else:
+        session.clear()
+        return redirect("/")
     
+@app.route("/p4", methods=['GET','POST'])
+def render_page4():
+
+    if request.method == "POST":
+        if session.get("answer3") is not None: #check if there is already a value for "answer1"
+            session.clear()
+            return redirect("/")
+        else:
+            if 'optradio' in request.form:
+                session["answer3"]=request.form['optradio']
+            else:
+                session["answer3"] = "None"
+        
+        return render_template("page4.html")
+    else:
+        session.clear()
+        return redirect("/")
+
+@app.route("/p5", methods=['GET','POST'])
+def render_page5():
+
+    if request.method == "POST":
+        if session.get("answer4") is not None: #check if there is already a value for "answer2"
+            session.clear()
+            return redirect("/")
+        else:
+            if 'optradio' in request.form:
+                session["answer4"]=request.form['optradio']
+            else:
+                session["answer4"] = "None"
+
+            score = 0
+            if session["answer1"] == "3":
+                score += 1
+            if session["answer2"] == "1":
+                score += 1
+            if session["answer3"] == "4":
+                score += 1
+            if session["answer4"] == "2":
+                score += 1
+
+            return render_template("page5.html", score = score)
+    else:
+        session.clear()
+        return redirect("/")
+
 if __name__=="__main__":
     app.run(debug=True)
